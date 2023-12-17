8dd1180348607dc63ce52950b1b5bccd4afb3b57
nasuda <nasuda908@gmail.com>
2023-12-17 21:44:35 +0900

    minimal config to display "react"

---
+++ esbuid-jsx/src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <h1>hello esbuild</h1>
+)
+
+console.log('app.jsx')
